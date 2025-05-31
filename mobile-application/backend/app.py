e1d96dd6d3db1fbd32c3a012a6548def74a90dbb
Saransh <saini.saransh03@gmail.com>
2025-05-31 16:48:08 +0530

    build sh added

--- mobile-application/backend/app.py
+++ mobile-application/backend/app.py
@@ -684,4 +684,5 @@ def try_compact_date(digits):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))  # Use Cloud Run's PORT
+    app.run(host="0.0.0.0", port=port)
